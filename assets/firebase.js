17981fe61f434522419171ec3a09611938fdfd8b
Vivek Sen <betu830573@gmail.com>
2026-06-09 21:16:32 +0530

    Initialize Firebase app and Firestore

--- assets/firebase.js
+++ assets/firebase.js
@@ -1,4 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAmpWiPU4RYNJD2XDrc7cZoP7P2yJAfIIc",
   authDomain: "book-bank-900cc.firebaseapp.com",
@@ -7,4 +11,9 @@ const firebaseConfig = {
   messagingSenderId: "661033796741",
   appId: "1:661033796741:web:a893c4097f1abfe31e55ec",
   measurementId: "G-01WNM75B9C"
+  
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const firestore = getFirestore(app);
