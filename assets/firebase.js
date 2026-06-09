0f04a38467e27abae406c4a07a25310c25327f5f
Vivek Sen <betu830573@gmail.com>
2026-06-09 23:04:37 +0530

    Update firebase.js

--- assets/firebase.js
+++ assets/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAmpWiPU4RYNJD2XDrc7cZoP7P2yJAfIIc",
   authDomain: "book-bank-900cc.firebaseapp.com",
   projectId: "book-bank-900cc",
-  storageBucket: "book-bank-900cc.firebasestorage.app",
+  storageBucket: "book-bank-900cc.appspot.com",
   messagingSenderId: "661033796741",
   appId: "1:661033796741:web:a893c4097f1abfe31e55ec",
   measurementId: "G-01WNM75B9C"
