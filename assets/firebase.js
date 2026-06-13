d8ae7984181129bc651f0b6a05ea943de08bba15
Vivek Sen <betu830573@gmail.com>
2026-06-13 11:18:01 +0530

    Update firebase.js

--- assets/firebase.js
+++ assets/firebase.js
@@ -1,7 +1,6 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAmpWiPU4RYNJD2XDrc7cZoP7P2yJAfIIc",
@@ -11,9 +10,9 @@ const firebaseConfig = {
   messagingSenderId: "661033796741",
   appId: "1:661033796741:web:a893c4097f1abfe31e55ec",
   measurementId: "G-01WNM75B9C"
-  
 };
 
 const app = initializeApp(firebaseConfig);
 
+export const auth = getAuth(app);
 export const firestore = getFirestore(app);
