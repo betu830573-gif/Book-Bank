05e64eb22d8727ec53627811a23ea139a06038f2
Vivek Sen <betu830573@gmail.com>
2026-06-15 22:48:00 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -12,11 +12,24 @@
  * If you leave it as is, the project will automatically fall back to browser localStorage.
  */
 
-window.FIREBASE_CONFIG = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAmpWiPU4RYNJD2XDrc7cZoP7P2yJAfIIc",
+  authDomain: "book-bank-900cc.firebaseapp.com",
+  projectId: "book-bank-900cc",
+  storageBucket: "book-bank-900cc.firebasestorage.app",
+  messagingSenderId: "661033796741",
+  appId: "1:661033796741:web:a893c4097f1abfe31e55ec",
+  measurementId: "G-01WNM75B9C"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
