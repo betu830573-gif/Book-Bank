60e9a1467f46f2569915590471f0ad188c6a13c5
Vivek Sen <betu830573@gmail.com>
2026-06-16 20:46:56 +0530

    Fix import statement and format firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,27 @@
 // Firebase SDK Imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
 
-import { 
-    getAuth 
+import {
+    getAuth
 } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
 
 // Firebase Config
 const firebaseConfig = {
+
     apiKey: "AIzaSyAmpWiPU4RYNJD2XDrc7cZoP7P2yJAfIIc",
+
     authDomain: "book-bank-900cc.firebaseapp.com",
+
     projectId: "book-bank-900cc",
+
     storageBucket: "book-bank-900cc.firebasestorage.app",
+
     messagingSenderId: "661033796741",
+
     appId: "1:661033796741:web:a893c4097f1abfe31e55ec",
+
     measurementId: "G-01WNM75B9C"
+
 };
 
 // Initialize Firebase
