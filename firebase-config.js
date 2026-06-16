aca2c7d7cee253655e69efa9721788a3e3761a9f
Vivek Sen <betu830573@gmail.com>
2026-06-16 21:17:41 +0530

    Refactor Firebase config to remove duplicates
    Removed redundant Firebase configuration properties.

--- firebase-config.js
+++ firebase-config.js
@@ -7,20 +7,15 @@ import {
 
 // Firebase Config
 const firebaseConfig = {
-
-    apiKey: "AIzaSyAmpWiPU4RYNJD2XDrc7cZoP7P2yJAfIIc",
-
-    authDomain: "book-bank-900cc.firebaseapp.com",
-
-    projectId: "book-bank-900cc",
-
-    storageBucket: "book-bank-900cc.firebasestorage.app",
-
-    messagingSenderId: "661033796741",
-
-    appId: "1:661033796741:web:a893c4097f1abfe31e55ec",
-
-    measurementId: "G-01WNM75B9C"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAmpWiPU4RYNJD2XDrc7cZoP7P2yJAfIIc",
+  authDomain: "book-bank-900cc.firebaseapp.com",
+  projectId: "book-bank-900cc",
+  storageBucket: "book-bank-900cc.firebasestorage.app",
+  messagingSenderId: "661033796741",
+  appId: "1:661033796741:web:a893c4097f1abfe31e55ec",
+  measurementId: "G-01WNM75B9C"
 
 };
 
