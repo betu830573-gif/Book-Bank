ea35df2019dc7a574a1b1f50de54c625703f461b
Vivek83 <betu830573@gmail.com>
2026-07-09 23:05:32 +0530

    Fix login loop issue and firebase configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,6 @@
-// Firebase SDK Imports
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-
-import {
-    getAuth
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
-
 // Firebase Config
-const firebaseConfig = {
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyAmpWiPU4RYNJD2XDrc7cZoP7P2yJAfIIc",
   authDomain: "book-bank-900cc.firebaseapp.com",
   projectId: "book-bank-900cc",
@@ -16,11 +8,4 @@ const firebaseConfig = {
   messagingSenderId: "661033796741",
   appId: "1:661033796741:web:a893c4097f1abfe31e55ec",
   measurementId: "G-01WNM75B9C"
-
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Export Auth
-export const auth = getAuth(app);
